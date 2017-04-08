a6f84c2cee6781389381a34ec67797bed12a3173
A0148544Y <e0013421@u.nus.edu>
2017-04-08 19:55:34 +0800

    4a: ioctl hello msg

--- onebyte.c
+++ onebyte.c
@@ -7,9 +7,13 @@
 #include <linux/fs.h>
 #include <linux/proc_fs.h>
 #include <asm/uaccess.h>
+#include <linux/ioctl.h>
 
 #define MAJOR_NUMBER 61
 #define MAX_SIZE 4194304
+#define SCULL_IOC_MAGIC 'k'
+#define SCULL_IOC_MAXNR 14
+#define SCULL_HELLO _IO(SCULL_IOC_MAGIC, 1)
 
 /* forward declaration */
 int onebyte_open(struct inode *inode, struct file *filep);
@@ -17,6 +21,7 @@ int onebyte_release(struct inode *inode, struct file *filep);
 ssize_t onebyte_read(struct file *filep, char *buf, size_t count, loff_t *f_pos);
 ssize_t onebyte_write(struct file *filep, const char *buf, size_t count, loff_t *f_pos);
 loff_t onebyte_llseek(struct file *filep, loff_t off, int whence);
+long onebyte_ioctl(struct file *filep, unsigned int cmd, unsigned long arg);
 static void onebyte_exit(void);
 
 /* definition of file_operation structure */
@@ -25,7 +30,8 @@ struct file_operations onebyte_fops = {
 	write: onebyte_write,
 	open: onebyte_open,
 	release: onebyte_release,
-	llseek: onebyte_llseek
+	llseek: onebyte_llseek,
+	unlocked_ioctl: onebyte_ioctl 
 };
 
 char *data = NULL;
@@ -82,6 +88,39 @@ loff_t onebyte_llseek(struct file *filep, loff_t f_pos, int whence)
 	if (new_pos < 0) return -EINVAL;
 	filep->f_pos = new_pos;
 	return new_pos;
+}
+
+long onebyte_ioctl(struct file *filep, unsigned int cmd, unsigned long arg)
+{
+	int err = 0;
+	int retval = 0;
+
+	/*
+	* extract the type and number bitfields, and don't decode
+	* wrong cmds: return ENOTTY (inappropriate ioctl) before access_ok()
+	*/
+	if (_IOC_TYPE(cmd) != SCULL_IOC_MAGIC) return -ENOTTY;
+	if (_IOC_NR(cmd) > SCULL_IOC_MAXNR) return -ENOTTY;
+
+	/*
+	* the direction is a bitmask, and VERIFY_WRITE catches R/W
+	* transfers. `Type' is user‐oriented, while
+	* access_ok is kernel‐oriented, so the concept of "read" and
+	* "write" is reversed
+	*/
+	if (_IOC_DIR(cmd) & _IOC_READ)
+		err = !access_ok(VERIFY_WRITE, (void __user *)arg, _IOC_SIZE(cmd));
+	else if (_IOC_DIR(cmd) & _IOC_WRITE)
+		err = !access_ok(VERIFY_READ, (void __user *)arg, _IOC_SIZE(cmd));
+	if (err) return -EFAULT;
+	switch(cmd) {
+		case SCULL_HELLO:
+			printk(KERN_WARNING "hello\n");
+			break;
+		default:  /* redundant, as cmd was checked against MAXNR */
+			return -ENOTTY;
+	}
+	return retval;
 }
 
 static int onebyte_init(void)
